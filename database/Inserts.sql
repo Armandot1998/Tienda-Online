3617a7f45988c6c421de61b4cace9061c44be06b
unknown <ITSYADM@GORI13.itsy.edu.ec>
2017-11-29 08:55:29 -0500

    Armando
    script de insert

---
+++ database/Inserts.sql
@@ -0,0 +1,11 @@
+SHOW DATABASES;
+USE ´tiendaOnline´;
+SHOW TABLES;
+
+INSERT INTO ´TipoProducto´ VALUES
+( 1, ´Este producto es un dispositivo de comunicacion´ ),
+(2, ´Este producto es un dispositivo de comunicacion´),
+(3, ´Este producto es un dispositivo de comunicacion´);
+
+
+
