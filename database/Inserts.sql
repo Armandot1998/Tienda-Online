ee94f09c3a7459c6f7f1b1fa62e6a697d91eea8b
Armandot1998 <33724801+Armandot1998@users.noreply.github.com>
2017-11-30 20:54:44 -0500

    Armando
    Inserts principales

---
+++ database/Inserts.sql
@@ -0,0 +1,19 @@
+INSERT INTO TipoProducto values (
+    1, 'Telefono Celular');
+
+INSERT INTO TipoProducto values (     
+    2, 'Televisor');
+INSERT INTO TipoProducto values (     
+    3, 'Ordenador');
+
+INSERT INTO Cargo values(
+    1, 'Cliente');
+
+INSERT INTO Cargo values (
+    2, 'Vendedor');
+
+INSERT INTO Cargo values (
+    3, 'gerente');
+INSERT INTO Cargo values (    
+    4, 'Tecnico');
+
