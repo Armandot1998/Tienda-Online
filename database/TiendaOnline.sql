669dd6f6e7fd1da83a1c803965f9e64f1541f2b1
Armando1998c <31851660+Armando1998c@users.noreply.github.com>
2017-12-06 14:47:08 -0500

    Armando
    database

--- database/TiendaOnline.sql
+++ database/TiendaOnline.sql
@@ -31,7 +31,7 @@ CREATE TABLE `Usuarios` (
   `clave` VARCHAR(45) NOT NULL,
   `idCargo` INT NOT NULL,
   PRIMARY KEY (`idUsuarios`),
-    FOREIGN KEY (`CargoidCargo`)
+    FOREIGN KEY (`idCargo`)
     REFERENCES `Cargo` (`idCargo`))
 ENGINE = InnoDB;
 
@@ -56,3 +56,53 @@ CREATE TABLE `Factura` (
     FOREIGN KEY (`idUsuarios`)
     REFERENCES `Usuarios` (`idUsuarios`))
 ENGINE = InnoDB;
+
+INSERT INTO TipoProducto values (
+    1, 'Telefono Celular');
+
+INSERT INTO TipoProducto values (     
+    2, 'Televisor');
+INSERT INTO TipoProducto values (     
+    3, 'Ordenador');
+
+INSERT INTO Cargo values(
+    1, 'Cliente');
+
+INSERT INTO Cargo values (
+    2, 'Vendedor');
+
+INSERT INTO Cargo values (
+    3, 'gerente');
+INSERT INTO Cargo values (    
+    4, 'Tecnico');
+
+
+INSERT INTO Productos  values(
+    1, 'Galaxy J2 Prime', 'Samsung', 120.00, 1
+);
+
+
+INSERT INTO Productos  values(
+    2, 'Televisor Smart Tv Uhd 4K', 'Samsung', 1469.00, 1
+);
+
+
+INSERT INTO Productos  values(
+    3, 'PC Gamer Core I7', 'Dell', 900.00, 1
+);
+
+INSERT INTO Usuarios values(
+    1, 'Armando Telmo', 'Cajilema Cuji', 1726850660, 'armandotcajilema@gmail.com', '12345', 1
+);
+
+INSERT INTO DetalleDeFactura values(
+    1, 1, 1
+);
+
+INSERT INTO DetalleDeFactura values(
+    2, 3, 1
+);
+
+INSERT INTO Factura values(
+    1, '2017-11-30', 2, 1
+);
